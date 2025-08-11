b6c9a802c48222d62873b730ea5e979343efed2b
dt3605 <daviteod2019@icloud.com>
2025-08-11 11:57:03 -0300

    mario.c

--- mario.c
+++ mario.c
@@ -1,29 +1,44 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int h;
+    int height;
+
+    // Ask for height until it's between 1 and 8
     do
     {
-        h = get_int("Height: ");
-    } while (h < 1 || h > 8);
+        printf("Height: ");
+        scanf("%d", &height);
+    }
+    while (height < 1 || height > 8);
 
-    for (int row = 1; row <= h; row++)
+    // Print the pyramid
+    for (int i = 1; i <= height; i++)
     {
-        for (int s = 0; s < h - row; s++)
+        // Left spaces
+        for (int j = 0; j < height - i; j++)
         {
             printf(" ");
         }
-        for (int j = 0; j < row; j++)
+
+        // Left hashes
+        for (int j = 0; j < i; j++)
         {
             printf("#");
         }
+
+        // Gap
         printf("  ");
-        for (int j = 0; j < row; j++)
+
+        // Right hashes
+        for (int j = 0; j < i; j++)
         {
             printf("#");
         }
+
         printf("\n");
     }
+
+    return 0;
 }
+
